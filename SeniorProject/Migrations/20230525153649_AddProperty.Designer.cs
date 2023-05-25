5196a1ed1f4afbd8604a8e7fe85e5d6164913421
Michaelrbacu <michaelrbacu2@gmail.com>
2023-05-25 11:38:47 -0400

    sql

--- SeniorProject/Migrations/20230525153649_AddProperty.Designer.cs
+++ SeniorProject/Migrations/20230525153649_AddProperty.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SeniorProject.Models;
 
@@ -11,9 +12,11 @@ using SeniorProject.Models;
 namespace SeniorProject.Migrations
 {
     [DbContext(typeof(EmailContext))]
-    partial class EmailContextModelSnapshot : ModelSnapshot
+    [Migration("20230525153649_AddProperty")]
+    partial class AddProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
