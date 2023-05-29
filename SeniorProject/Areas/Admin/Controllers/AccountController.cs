d1b41311a339abeba8f6239bdb2f1e9c2de89525
farhanmf1 <134565990+farhanmf1@users.noreply.github.com>
2023-05-29 18:21:06 -0400

    email confirmation

--- SeniorProject/Areas/Admin/Controllers/AccountController.cs
+++ SeniorProject/Areas/Admin/Controllers/AccountController.cs
@@ -1,22 +1,25 @@
-﻿using SeniorProject.Models;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using SeniorProject.Models;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using System.Text.Encodings.Web;
 
 namespace SeniorProject.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IEmailSender _emailSender;
 
-        private UserManager<IdentityUser> UserManager { get; set; }
-        private SignInManager<IdentityUser> SignInManager { get; set; }
-
-
-        public AccountController(UserManager<IdentityUser> UserManager, SignInManager<IdentityUser> SignInManager)
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IEmailSender emailSender)
         {
-            this.UserManager = UserManager;
-            this.SignInManager = SignInManager;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _emailSender = emailSender;
         }
 
         [Route("[area]/[controller]/[action]")]
@@ -25,39 +28,55 @@ namespace SeniorProject.Areas.Admin.Controllers
         {
             return View();
         }
+
         [HttpPost]
         public async Task<IActionResult> Register(RegisterModel model)
         {
             if (ModelState.IsValid)
             {
-                IdentityUser identityUser = new IdentityUser();
-                identityUser.UserName = model.Email;
-                IdentityResult identityResult = await UserManager.CreateAsync(identityUser, model.Password);
+                IdentityUser identityUser = new IdentityUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email
+                };
+
+                IdentityResult identityResult = await _userManager.CreateAsync(identityUser, model.Password);
                 if (identityResult.Succeeded)
                 {
-                    await SignInManager.SignInAsync(identityUser, isPersistent: false);
-                }
-                return RedirectToAction("Index", "Home");
+                    // Generate email confirmation token and send it
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(identityUser);
+                    var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = identityUser.Id, code = code }, protocol: HttpContext.Request.Scheme);
 
+                    // Customize the email message
+                    var emailMessage = $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.";
+
+                    await _emailSender.SendEmailAsync(model.Email, "Confirm your email", emailMessage);
+
+                    // Redirect the user to the email confirmation sent page
+                    return View("EmailConfirmationSent");
+                }
+
+                foreach (var error in identityResult.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
             }
-            else
-            {
-                return View(model);
-            }
+            return View(model);
         }
+
         [Route("[area]/[controller]/[action]")]
         [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
+
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel model)
         {
             if (ModelState.IsValid)
             {
-
-                Microsoft.AspNetCore.Identity.SignInResult identityResult = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, isPersistent: false, lockoutOnFailure: false);
+                Microsoft.AspNetCore.Identity.SignInResult identityResult = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, isPersistent: false, lockoutOnFailure: false);
                 if (identityResult.Succeeded)
                 {
                     return RedirectToAction("Index", "Home");
@@ -66,6 +85,7 @@ namespace SeniorProject.Areas.Admin.Controllers
             ModelState.AddModelError("", "Invalid Username/Password");
             return View();
         }
+
         public IActionResult AccessDenied()
         {
             return View();
@@ -74,11 +94,27 @@ namespace SeniorProject.Areas.Admin.Controllers
         [HttpPost]
         public async Task<IActionResult> LogOut()
         {
-            await SignInManager.SignOutAsync();
+            await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
 
+        [HttpGet]
+        public async Task<IActionResult> ConfirmEmail(string userId, string code)
+        {
+            if (userId == null || code == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
 
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                throw new ApplicationException($"Unable to find user with ID '{userId}'.");
+            }
+
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+            return View(result.Succeeded ? "ConfirmEmailSuccess" : "ConfirmEmailFailure");
+        }
 
         public IActionResult Index()
         {
