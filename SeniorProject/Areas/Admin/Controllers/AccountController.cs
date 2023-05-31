df97eba6a4b1016b91389fcdd61cfb899855e61b
Michaelrbacu <michaelrbacu2@gmail.com>
2023-05-31 01:11:51 -0400

    revert changes

--- SeniorProject/Areas/Admin/Controllers/AccountController.cs
+++ SeniorProject/Areas/Admin/Controllers/AccountController.cs
@@ -1,25 +1,22 @@
-﻿using System.Threading.Tasks;
+﻿using SeniorProject.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using SeniorProject.Models;
-using Microsoft.AspNetCore.Identity.UI.Services;
-using System.Text.Encodings.Web;
 
 namespace SeniorProject.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class AccountController : Controller
     {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly IEmailSender _emailSender;
 
-        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IEmailSender emailSender)
+        private UserManager<IdentityUser> UserManager { get; set; }
+        private SignInManager<IdentityUser> SignInManager { get; set; }
+
+
+        public AccountController(UserManager<IdentityUser> UserManager, SignInManager<IdentityUser> SignInManager)
         {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _emailSender = emailSender;
+            this.UserManager = UserManager;
+            this.SignInManager = SignInManager;
         }
 
         [Route("[area]/[controller]/[action]")]
@@ -28,55 +25,39 @@ namespace SeniorProject.Areas.Admin.Controllers
         {
             return View();
         }
-
         [HttpPost]
         public async Task<IActionResult> Register(RegisterModel model)
         {
             if (ModelState.IsValid)
             {
-                IdentityUser identityUser = new IdentityUser
-                {
-                    UserName = model.Email,
-                    Email = model.Email
-                };
-
-                IdentityResult identityResult = await _userManager.CreateAsync(identityUser, model.Password);
+                IdentityUser identityUser = new IdentityUser();
+                identityUser.UserName = model.Email;
+                IdentityResult identityResult = await UserManager.CreateAsync(identityUser, model.Password);
                 if (identityResult.Succeeded)
                 {
-                    // Generate email confirmation token and send it
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(identityUser);
-                    var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = identityUser.Id, code = code }, protocol: HttpContext.Request.Scheme);
-
-                    // Customize the email message
-                    var emailMessage = $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.";
-
-                    await _emailSender.SendEmailAsync(model.Email, "Confirm your email", emailMessage);
-
-                    // Redirect the user to the email confirmation sent page
-                    return View("EmailConfirmationSent");
+                    await SignInManager.SignInAsync(identityUser, isPersistent: false);
                 }
+                return RedirectToAction("Index", "Home");
 
-                foreach (var error in identityResult.Errors)
-                {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                }
             }
-            return View(model);
+            else
+            {
+                return View(model);
+            }
         }
-
         [Route("[area]/[controller]/[action]")]
         [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
-
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel model)
         {
             if (ModelState.IsValid)
             {
-                Microsoft.AspNetCore.Identity.SignInResult identityResult = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, isPersistent: false, lockoutOnFailure: false);
+
+                Microsoft.AspNetCore.Identity.SignInResult identityResult = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, isPersistent: false, lockoutOnFailure: false);
                 if (identityResult.Succeeded)
                 {
                     return RedirectToAction("Index", "Home");
@@ -85,7 +66,6 @@ namespace SeniorProject.Areas.Admin.Controllers
             ModelState.AddModelError("", "Invalid Username/Password");
             return View();
         }
-
         public IActionResult AccessDenied()
         {
             return View();
@@ -94,27 +74,11 @@ namespace SeniorProject.Areas.Admin.Controllers
         [HttpPost]
         public async Task<IActionResult> LogOut()
         {
-            await _signInManager.SignOutAsync();
+            await SignInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
         }
 
-        [HttpGet]
-        public async Task<IActionResult> ConfirmEmail(string userId, string code)
-        {
-            if (userId == null || code == null)
-            {
-                return RedirectToAction("Index", "Home");
-            }
 
-            var user = await _userManager.FindByIdAsync(userId);
-            if (user == null)
-            {
-                throw new ApplicationException($"Unable to find user with ID '{userId}'.");
-            }
-
-            var result = await _userManager.ConfirmEmailAsync(user, code);
-            return View(result.Succeeded ? "ConfirmEmailSuccess" : "ConfirmEmailFailure");
-        }
 
         public IActionResult Index()
         {
--- SeniorProject/Models/RegisterModel.cs
+++ SeniorProject/Models/RegisterModel.cs
@@ -1,23 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = Microsoft.Build.Framework.RequiredAttribute;
 
 namespace SeniorProject.Models
 {
     public class RegisterModel
     {
         [Required]
-        [EmailAddress]
         public string Email { get; set; }
-
         [Required]
         [DataType(DataType.Password)]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at most {1} characters long.", MinimumLength = 6)]
+        [Compare("ConfirmPassword")]
         public string Password { get; set; }
+        [Required]
 
-        [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
 
-        public bool IsEmailConfirmed { get; set; }
+
     }
 }
