7899d6bc2dcb1f1d7a35ff553edc8ca50610158c
Michaelrbacu <michaelrbacu2@gmail.com>
2023-06-03 03:07:29 -0400

    Event Class

---
+++ SeniorProject/Models/Event.cs
@@ -0,0 +1,11 @@
+﻿namespace SeniorProject.Models
+{
+    public class Event
+    {
+        public string? EventName { get; set; }
+        public string? EventStart { get; set; }
+        public string? EventEnd { get; set; }
+
+
+    }
+}
